fb4009698eeef7efa4ac0561fb1211f280dff29c
tjcrowell <mrcrowell79@gmail.com>
2021-10-16 23:01:20 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Data from './data'
+import Product from './components/Product'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="grid-container">
+			<header className="row">
+				<div>
+					<a className="brand" href="/"
+						><span> Nutech </span> Technologies, LLC</a
+					>
+				</div>
+				<div>
+					<a href="/cart">Cart</a>
+					<a href="/signin">Sign In</a>
+				</div>
+			</header>
+			<main>
+				<div className="row center">
+          {Data.products.map((product) => (
+            /*first element after map should have a key attribute*/
+            /*Passing the product component to the product array here*/
+            <Product roduct key={product._id} product={product}></Product>
+            ))}
+				</div>
+			</main>
+			<footer className="row center">All right reserved</footer>
+		</div>
   );
 }
 
